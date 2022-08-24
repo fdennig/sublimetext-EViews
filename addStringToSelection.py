5429792d149f7da7e3e7ad740be9a84dc97a13fb
francis dennig <fdennig@gmail.com>
2022-08-24 11:39:45 -0400

    updated addstringtoselection
    now it also takes a suffix as a second argument, after the prefix

--- addStringToSelection.py
+++ addStringToSelection.py
@@ -1,22 +1,55 @@
 import sublime
 import sublime_plugin
 
-class PromptAddStringToSelectionCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        # sublime.set_clipboard('that')
-        self.window.show_input_panel("Add string to selection", "", self.on_done, None, None)
-
-    def on_done(self, text):
-        try:
-            if self.window.active_view():
-                self.window.active_view().run_command("add_string_to_selection", {"strtoadd": text})
-        except ValueError:
-            pass
-
 class AddStringToSelectionCommand(sublime_plugin.TextCommand):
-    def run(self,edit,strtoadd):
+    def run(self,edit, prefix, suffix):
         editedstr = ''
         for i, tx in enumerate(self.view.sel()):
-            editedstr += strtoadd + self.view.substr(tx) + " "
+            editedstr += prefix + self.view.substr(tx) + suffix + " "
         sublime.set_clipboard(editedstr)
 
+    def input(self, args):
+        return PrefixInputHandler(self.view)
+
+class PrefixInputHandler(sublime_plugin.TextInputHandler):
+    def __init__(self,view):
+        self.view = view
+        self.prefixedList = []
+    # def name(self):
+    #     return "prefix"
+
+    def name(self):
+        return "prefix"
+
+    def placeholder(self):
+        return "Prefix"
+
+    def preview(self, text):
+        prefixedString = ''
+        prefixedList = []
+        for i, tx in enumerate(self.view.sel()):
+            prefixedString += text + self.view.substr(tx) + " \n"
+            prefixedList.append(text + self.view.substr(tx))
+        self.prefixedList = prefixedList
+        print(self.prefixedList)
+        return prefixedString
+
+    def next_input(self,args):
+        return SuffixInputHandler(self.view, self.prefixedList)
+
+class SuffixInputHandler(sublime_plugin.TextInputHandler):
+    def __init__(self,view, prefixedList):
+        self.view = view
+        self.prefixedList = prefixedList
+
+    def name(self):
+        return "suffix"
+
+    def placeholder(self):
+        return "Suffix"
+
+    def preview(self, text):
+        suffixedString = ''
+        for i, tx in enumerate(self.prefixedList):
+            suffixedString += tx + text + " \n"
+        return suffixedString
